253460eecb979e9e727476f2a355fa3605ebceb8
Owen Shepherd <414owen@gmail.com>
2018-10-28 01:22:40 +0100

    added '.lambda-calculus' span

--- ast.js
+++ ast.js
@@ -43,12 +43,12 @@ const classNameMap = {
 
 const span = (className, str) => `<span className="${className}">${str}</span>`;
 
-astToHtmlString = ast => astToString(ast, {
+const astToHtmlString = ast => span('lambda-calculus', astToString(ast, {
   ...(printTokens.reduce((acc, token) => {
     const className = classNameMap[token] || token;
     return ({ ...acc, [token]: val => span(className, val || token) });
   }, {}))
-});
+}));
 
 module.exports = {
   astNewApp,
